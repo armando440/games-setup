0da0c8f9189185b78e4fdff88bec7c4bfeb7bfbd
armando44o <armandovelazco2001@gmail.com>
2020-08-06 13:45:03 -0600

    implemented delete in service

--- data/games.js
+++ data/games.js
@@ -1,6 +1,6 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');//const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 
-//const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 const url = process.env.DB_URL;
@@ -44,6 +44,7 @@ const createGame = (game) => {
 const deleteGame = (id) => {
     const iou = new Promise((resolve, reject) => {
         MongoClient.connect(url, options, (err, client) => {
+            assert.equal(err, null);
             const db = client.db(db_name);
             const collection = db.collection(col_name);
             collection.findOneAndDelete({_id: new ObjectID(id) }, (err, result) => {
--- routes/api/games.js
+++ routes/api/games.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 const {
     readGames, 
-    createGame
+    createGame,
+    deleteGame
 } = require('../../data/games');
 
 /* GET users listing. */
@@ -12,13 +13,18 @@ router.get('/', async function(req, res, next) {
 });
 
 router.post('/', async function(req, res) {
- /* //extract request body data
+  //extract request body data
   const body = req.body;
   //use the DAL to create using that data
   const data = await createGame(body);
   //send the results
-  res.send(data);*/
-  res.send(await createGame(req.body));
-})
+  res.send(data);
+  //res.send(await createGame(req.body)); // this is the same as the code above but it is condenced into one line same thing though. 
+});
+
+router.delete('/:id', async function(req, res) {
+  const data = await deleteGame(req.params.id);//take notes on this and get more familiar with req.params
+  res.send(data);
+});
 
 module.exports = router;
