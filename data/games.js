d8f08b8c1f12ee72dec44aa1cd427fbbb3c527de
armando44o <armandovelazco2001@gmail.com>
2020-08-07 17:00:42 -0600

    Put in upsert in service

--- data/games.js
+++ data/games.js
@@ -1,6 +1,5 @@
 const { MongoClient } = require('mongodb');//const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
-
 const assert = require('assert');
 
 const url = process.env.DB_URL;
@@ -32,7 +31,7 @@ const readGameByID = (id) => {
         MongoClient.connect(url, options, (err, client) => {
             assert.equal(err, null);
 
-            const db = client.connect.db(db_name);
+            const db = client.db(db_name);
             const collection = db.collection(col_name);
             collection.find({_id: new ObjectID(id) }).toArray((err, docs) =>{
                 assert.equal(err, null);
@@ -99,8 +98,9 @@ const deleteGame = (id) => {
 }
 
 module.exports ={
-    readGames,
     createGame,
+    readGames,
     upsertGame,
     deleteGame
+    
 }
--- routes/api/games.js
+++ routes/api/games.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const {
-    readGames, 
     createGame,
+    readGames, 
+    upsertGame,
     deleteGame
 } = require('../../data/games');
 
@@ -21,6 +22,13 @@ router.post('/', async function(req, res) {
   res.send(data);
   //res.send(await createGame(req.body)); // this is the same as the code above but it is condenced into one line same thing though. 
 });
+
+router.put('/:id', async function (req, res){
+      const body = req.body;
+      const id = req.params.id;
+      const data = await upsertGame(id, body);
+      res.send(data);
+})
 
 router.delete('/:id', async function(req, res) {
   const data = await deleteGame(req.params.id);//take notes on this and get more familiar with req.params
