90a0ddf733fe098535e9b8b7a1a868314c4795fc
armando44o <armandovelazco2001@gmail.com>
2020-08-11 14:10:01 -0600

    first heroku deployment

---
+++ public/precache-manifest.b6424c9ebbb2fafc033e68003856794b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c6e38927b08ca3428858c42cb97cbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03b94754b4d553d4c0da",
+    "url": "/static/css/main.631c3104.chunk.css"
+  },
+  {
+    "revision": "eec87eb09a11b1256387",
+    "url": "/static/js/2.14a7cabd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14a7cabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b94754b4d553d4c0da",
+    "url": "/static/js/main.516adc15.chunk.js"
+  },
+  {
+    "revision": "1aadcde5ef86e7287682",
+    "url": "/static/js/runtime-main.44d2f03b.js"
+  }
+]);
